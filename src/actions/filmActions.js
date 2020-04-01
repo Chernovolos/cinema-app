04957201072858665833be2a9a43f96c4a37866a
Maria <mariachernovolos@gmail.com>
2020-04-01 03:58:46 +0300

    [*] implemented data model for filmActions;

--- src/actions/filmActions.js
+++ src/actions/filmActions.js
@@ -1,17 +1,19 @@
 import axios from 'axios';
 import * as ACTION from "../actions/types";
+import Film  from "../models/film";
 
 export const getCurrentFilmsStart = () => ({type: ACTION.GET_CURRENT_FILMS_START});
 export const getCurrentFilmsSuccess = (films) => ({type: ACTION.GET_CURRENT_FILMS_SUCCESS, payload: films});
 export const getCurrentFilmsError = (error) => ({type: ACTION.GET_CURRENT_FILMS_ERROR, payload: error});
 export const filterFilms = (filter) => ({type: ACTION.FILTER_CURRENT_FILMS, payload: filter});
+export const setFilm = (film) => ({type: ACTION.SET_FILM, payload: film});
 
 export const getCurrentFilms = () => (dispatch) => {
     dispatch(getCurrentFilmsStart());
     axios("http://cinema-app-1.herokuapp.com/api/movie")
-        .then(result => {
-            console.log(result);
-            dispatch(getCurrentFilmsSuccess(result.data.movie))
+        .then(({data}) => {
+            let films = data.movie.map(movie => (new Film(movie)));
+            dispatch(getCurrentFilmsSuccess(films))
         })
         .catch(error => dispatch(getCurrentFilmsError(error)))
 };
