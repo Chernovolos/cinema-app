b461f39dd3b103b8012e49a1c1175b9ce8f2390f
Maria <mariachernovolos@gmail.com>
2020-04-01 18:47:49 +0300

    [*] added new styles;

--- src/component/Preloader.jsx
+++ src/component/Preloader.jsx
@@ -1,11 +1,13 @@
 import React from "react";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import { faCoffee} from "@fortawesome/free-solid-svg-icons";
 
 export default ({show}) => {
 
     return (
         show ?
             <div>
-                Loading...
+               <FontAwesomeIcon icon={faCoffee}/>
             </div>:
             null
     );
