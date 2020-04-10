12720698cb6eaad2e24dee51c7ba60b2d9eb66ab
Maria <mariachernovolos@gmail.com>
2020-04-10 17:51:36 +0300

    [+] added ticket page reducer;

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,11 @@
 import { combineReducers } from "redux";
 import mainPageReducer from "./mainPageReducer";
 import filmPageReducer from "./filmPageReducer";
+import ticketPageReducer from "./ticketPageReducer";
+
 
 export default combineReducers({
     mainPage: mainPageReducer,
-    filmPage: filmPageReducer
+    filmPage: filmPageReducer,
+    ticketPage: ticketPageReducer
 });
