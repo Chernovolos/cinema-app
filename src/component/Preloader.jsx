5e45d71ecb044b594e8e38cd0307cf82ce86d306
Maria <mariachernovolos@gmail.com>
2020-04-10 18:02:38 +0300

    [*] changed preloader;

--- src/component/Preloader.jsx
+++ src/component/Preloader.jsx
@@ -1,13 +1,13 @@
 import React from "react";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import { faCoffee} from "@fortawesome/free-solid-svg-icons";
+// import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+// import { faCoffee} from "@fortawesome/free-solid-svg-icons";
 
 export default ({show}) => {
 
     return (
         show ?
             <div>
-               <FontAwesomeIcon icon={faCoffee}/>
+               <h1>LOADING...LOADING</h1>
             </div>:
             null
     );
